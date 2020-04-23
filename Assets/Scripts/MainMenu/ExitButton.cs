86b8b5040d7b2153c7fce7138e7f3c44fe23b7b8
Ruslan Dudnikov <jerryletehen@gmail.com>
2020-04-23 15:27:14 +0000

    Merged in feature/add_main_menu (pull request #2)
    Add simple main menu
    
    * add ExitButton script that represents its behavior
    
    * add main menu with simple play and exit & start
    
    * ignore Rider files
    
    Approved-by: Ruslan Dudnikov <jerryletehen@gmail.com>

---
+++ Assets/Scripts/MainMenu/ExitButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CastleFight
+{
+    public class ExitButton : MonoBehaviour
+    {
+        [SerializeField] private Button btn;
+
+        private void Awake()
+        {
+            btn.onClick.AddListener(OnButtonClick);
+        }
+
+        private void OnDestroy()
+        {
+            btn.onClick.RemoveAllListeners();
+        }
+
+        private void OnButtonClick()
+        {
+            Application.Quit();
+            Debug.LogError("we are here");
+        }
+    }
+}
