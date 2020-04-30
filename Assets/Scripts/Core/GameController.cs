0126acc3bbcdf501b2a8b64619042fd79f87268c
vitalikbobrik <vitaly.bobrik@mobyrix.com>
2020-04-30 17:51:56 +0300

    add game controller

---
+++ Assets/Scripts/Core/GameController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace CastleFight.Core
+{
+    public class GameController : MonoBehaviour
+    {
+        private void Awake()
+        {
+            ManagerHolder.I.AddManager(this);
+        }
+    }
+}
+
