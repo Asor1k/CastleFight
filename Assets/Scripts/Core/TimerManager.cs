7b715c1590883cd0e66bdefa66b99c11bf2fd2da
Asor1k <roma.dotsenko@gmail.com>
2020-07-09 13:59:13 +0300

    Юниты спавнятся по таймеру. Визуализации пока нет

---
+++ Assets/Scripts/Core/TimerManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CastleFight.Core.EventsBus;
+using CastleFight.Core.EventsBus.Events;
+using UnityEngine;
+namespace CastleFight.Core {
+    public class TimerManager : MonoBehaviour
+    {
+        public float SpawnTimer => spawnTimer;
+
+        [SerializeField] private TimerConfig timerConfig;
+        private float spawnTimer;
+
+        public void Start()
+        {
+            EventBusController.I.Bus.Subscribe<GameSetReadyEvent>(OnGameStart);
+            ManagerHolder.I.AddManager(this);
+        }
+        public void OnGameStart(GameSetReadyEvent gameSetReadyEvent)
+        {
+            Init();
+        }
+        public void Init()
+        {
+            spawnTimer = timerConfig.SpawnTime;
+        }
+        public void Update()
+        {
+            spawnTimer -= Time.deltaTime;
+            if (spawnTimer <= 0)
+            {
+                spawnTimer = timerConfig.SpawnTime;
+                EventBusController.I.Bus.Publish(new SpawnUnitsEvent());
+            }
+        }
+        public void OnDestroy()
+        {
+            EventBusController.I.Bus.Unsubscribe<GameSetReadyEvent>(OnGameStart);
+        }
+
+    }
+}
--- Assets/Scripts/Unit/UnitSpawn.cs
+++ Assets/Scripts/Unit/UnitSpawn.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using CastleFight.Config;
 using CastleFight.Core.EventsBus;
+using CastleFight.Core.EventsBus.Events;
 using Core;
 using CastleFight.Core;
 
@@ -13,12 +14,13 @@ namespace CastleFight
         private Building building;
         private IUpdateManager updateManager;
         private float spawnDelay;
-        [SerializeField]  private float spawnTimer = 0;
+        [SerializeField] private float spawnTimer = 0;
         private bool buildingReady = false;
 
         private void Awake()
         {
             building.OnReady += OnBuildingReadyHandler;
+            EventBusController.I.Bus.Subscribe<SpawnUnitsEvent>(OnUnitsSpawn);
         }
 
         private void OnBuildingReadyHandler()
@@ -29,23 +31,31 @@ namespace CastleFight
             building.OnReady -= OnBuildingReadyHandler;
         }
 
-
+        private void OnUnitsSpawn(SpawnUnitsEvent spawnUnitsEvent)
+        {
+            if (building.SpawnBlocked) return;
+            SpawnUnit(building.SpawnPoint.position, building.Behavior.Team);
+        }
         private void Update()
         {
-            if(!buildingReady){return;}
+            /* if(!buildingReady){return;}
 
-            UpdateTimer();
+             UpdateTimer();
 
-            if(spawnTimer < 0 && !building.SpawnBlocked)
-            {
-                spawnTimer = spawnDelay;
-                SpawnUnit(building.SpawnPoint.position, building.Behavior.Team);
-            }
+             if(spawnTimer < 0 && !building.SpawnBlocked)
+             {
+                 spawnTimer = spawnDelay;
+                 SpawnUnit(building.SpawnPoint.position, building.Behavior.Team);
+             }*/
+        }
+        public void OnDestroy()
+        {
+            EventBusController.I.Bus.Unsubscribe<SpawnUnitsEvent>(OnUnitsSpawn);
         }
 
         private void UpdateTimer()
         {
-            if(spawnTimer >= 0)
+            if (spawnTimer >= 0)
                 spawnTimer -= Time.deltaTime;
         }
 
