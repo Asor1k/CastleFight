2136d4b946c5efc2fec56ca4310796de03adcbdb
MrDuden <unitydeveloper7@gmail.com>
2020-07-11 14:55:40 +0300

    Add gryphon level1 and level2, fix ghost double attack

---
+++ Assets/Scripts/Config/Units/Gryphons/GryphonLevel1Config.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CastleFight.Core;
+using UnityEngine;
+
+namespace CastleFight.Config
+{
+    [CreateAssetMenu(fileName = "GryphonLevel1", menuName = "Units/GryphonLevel1", order = 0)]
+    public class GryphonLevel1Config : BaseUnitConfig
+    {
+        public override Unit Create(Team team)
+        {
+            var unit = Pool.I.Get<GryphonLevel1>();
+
+            if(unit == null)
+            {
+                unit = Instantiate(prefab) as GryphonLevel1;
+                unit.Init(this, team);
+                unit.gameObject.SetActive(true);
+            }
+
+            unit.gameObject.layer = (int)team;
+            return unit;
+        }
+    }
+}
