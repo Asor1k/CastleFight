d594af8d2650a5b312f458d5f4d10d7bf6645f62
MrDuden <unitydeveloper7@gmail.com>
2020-07-11 14:24:47 +0300

    Add ghost level 1 and 2

---
+++ Assets/Scripts/Config/Units/Ghosts/GhostLevel1Config.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CastleFight.Core;
+using UnityEngine;
+
+namespace CastleFight.Config
+{
+    [CreateAssetMenu(fileName = "GhostLevel1", menuName = "Units/GhostLevel1", order = 0)]
+    public class GhostLevel1Config : BaseUnitConfig
+    {
+        public override Unit Create(Team team)
+        {
+            var unit = Pool.I.Get<GhostLevel1>();
+
+            if(unit == null)
+            {
+                unit = Instantiate(prefab) as GhostLevel1;
+                unit.Init(this, team);
+                unit.gameObject.SetActive(true);
+            }
+
+            unit.gameObject.layer = (int)team;
+            return unit;
+        }
+    }
+}
