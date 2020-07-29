1fd9446ece0d32ae392d0fbf897e79a2ff97fb8c
MrDuden <unitydeveloper7@gmail.com>
2020-07-29 15:17:00 +0300

    Merge ifx

--- Assets/Scripts/Unit/Units/Unit.cs
+++ Assets/Scripts/Unit/Units/Unit.cs
@@ -6,24 +6,24 @@ using System.Collections;
 using CastleFight.Core.EventsBus;
 using CastleFight.Core;
 using System.Threading.Tasks;
+using CastleFight.Skills;
 
 namespace CastleFight
 {
-    public abstract class  Unit : MonoBehaviour
+    public abstract class Unit : MonoBehaviour
     {
         public event Action OnInit;
         public event Action OnKilled;
         public event Action OnReset;
 
-        public bool Alive{get{return alive;}}
-        public BaseUnitConfig Config{get{return config;}}
-        public float CurrentSpeed{get{return agent.Speed;}}
-        public virtual float Speed{get{return stats.Speed;}}
-        public virtual float EnemyDetectRange{get{return config.EnemyDetectRange;}}
-        public virtual float AttackDistance{get{return config.AttackDistance;}}
-        
+        public bool Alive { get { return alive; } }
+        public BaseUnitConfig Config { get { return config; } }
+        public float CurrentSpeed { get { return agent.Speed; } }
+        public UnitStats Stats { get { return stats; } }
+        public Skill AttackSkill { get { return attackSkill; } }
+
         [SerializeField]
-        protected Agent agent; 
+        protected Agent agent;
         [SerializeField]
         protected Collider collider;
         [SerializeField]
@@ -34,76 +34,90 @@ namespace CastleFight
         protected BaseUnitConfig config;
         [SerializeField]
         protected UnitHealthBar healthBarCanvas;
+        [SerializeField]
+        protected Skill attackSkill;
 
         protected Team team;
         protected IDamageable target;
-        protected int hp;
-        protected int maxHp;
         protected bool alive = true;
         protected bool readyToAttack = true;
 
         private GoldManager goldManager;
-        
+        private Stat attackDelay;
+        private Stat damage;
+
         public virtual void Init(BaseUnitConfig config, Team team)
         {
             this.team = team;
             gameObject.layer = (int)team;
             this.config = config;
-            maxHp = config.MaxHp;
-            hp = maxHp;
             alive = true;
-            stats.Init(config.MaxHp, config.Speed);
-            stats.OnDamaged += OnUnitDamaged;
+            stats.Init(config.Stats.ToArray());
+            InitPassiveAbilities();
+            stats.OnHpChanged += OnUnitDamaged;
             goldManager = ManagerHolder.I.GetManager<GoldManager>();
-            agent.Init(config);
+            agent.Init(this);
+            attackSkill.Init(this);
+            attackDelay = (Stat)stats.GetStat(StatType.AttackDelay);
+            damage = (Stat)stats.GetStat(StatType.Damage);
             OnInit?.Invoke();
+        }
+
+        private void InitPassiveAbilities()
+        {
+            foreach (var ability in config.Abilities)
+            {
+                ability.Activate(this);
+            }
         }
 
         public virtual void MoveTo(Vector3 point)
         {
-            if(!alive) return;
+            if (!alive) return;
 
             agent.MoveTo(point);
         }
 
         public virtual void Stop()
         {
-            if(!alive) return;
-            
+            if (!alive) return;
+
             agent.Stop();
         }
-        
+
         public virtual void Attack(IDamageable target)
         {
-            if(!target.Alive || !alive || !readyToAttack) return;
+            if (!target.Alive || !alive || !readyToAttack) return;
             readyToAttack = false;
             StartAttackCooldown();
             agent.LookAt(target.Transform);
-            
-            animationController.Attack(()=>{
-                target.TakeDamage(config.Damage);
-                if (target.Type == TargetType.Building || target.Type == TargetType.Castle)
-                {
-                    int gold = GetGoldPerHit();
-                    goldManager.MakeGoldChange(gold, (Team)gameObject.layer);
-                    if (gameObject.layer == (int)Team.Team1)
-                        goldManager.InitGoldAnim(gold,target.Transform);
-                }
+            if (target.Type == TargetType.Building || target.Type == TargetType.Castle)
+            {
+                int gold = GetGoldPerHit();
+                goldManager.MakeGoldChange(gold, (Team)gameObject.layer);
+                if (gameObject.layer == (int)Team.Team1)
+                    InitGoldAnim(config.Cost);
+            }
+
+            animationController.Attack(() =>
+            {
+                attackSkill.SetTarget(target);
+                attackSkill.Execute();
             });
         }
-        
+
         private int GetGoldPerHit()
         {
-            return Mathf.RoundToInt(config.Damage * config.goldDmgFraction);
+            return Mathf.RoundToInt(damage.Value * config.goldDmgFraction);
         }
-        
+
         private void Kill()
         {
             alive = false;
             collider.enabled = false;
             agent.Disable();
             healthBarCanvas.Show(false);
-            goldManager.MakeGoldChange(config.Cost, (Team)gameObject.layer==Team.Team1?Team.Team2:Team.Team1);
+            goldManager.MakeGoldChange(config.Cost, (Team)gameObject.layer == Team.Team1 ? Team.Team2 : Team.Team1);
             EventBusController.I.Bus.Publish(new UnitDiedEvent(this));
             if (gameObject.layer == (int)Team.Team2)
             {
@@ -116,11 +130,9 @@ namespace CastleFight
         private async Task DelayDestroy()
         {
             await Task.Delay(3000);
-            //Destroy(gameObject);
-            gameObject.SetActive(false);
-            Pool.I.Put(this);
+            Destroy(gameObject);
         }
-        
+
         public void Reset()
         {
             collider.enabled = true;
@@ -129,17 +141,39 @@ namespace CastleFight
             OnReset?.Invoke();
         }
 
-        private void OnUnitDamaged()
+        private void OnUnitDamaged(Stat stat)
         {
-            if(stats.Hp <= 0)
+            if (stat.Value <= 0)
             {
                 Kill();
             }
         }
 
+        private void InitGoldAnim(int gold)
+        {
+            GoldAnim goldAnim = Pool.I.Get<GoldAnim>();
+            if (goldAnim == null)
+            {
+                goldAnim = Instantiate(goldManager.goldAnimPrefab, transform);
+            }
+            else
+            {
+                goldAnim.gameObject.SetActive(true);
+                goldAnim.transform.SetParent(transform);
+                goldAnim.transform.localPosition = Vector3.zero;
+            }
+            goldAnim.Init(gold);
+            StartCoroutine(DelayDestroyAnim(goldAnim));
+        }
+
+        private IEnumerator DelayDestroyAnim(GoldAnim anim)
+        {
+            yield return new WaitForSeconds(1);
+            anim.gameObject.SetActive(false);
+        }
         private async Task StartAttackCooldown()
         {
-            var miliseconds = config.AttackDelay * 1000;
+            var miliseconds = attackDelay.Value * 1000;
             await Task.Delay((int)miliseconds);
 
             readyToAttack = true;
