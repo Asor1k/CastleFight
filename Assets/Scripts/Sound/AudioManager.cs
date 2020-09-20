57cdbbf1d69782b0121a70a7c65ea5ad31bfcf81
Andrey <unitydeveloper7@gmail.com>
2020-09-20 15:33:50 +0300

    Фикс проверка на null если нету звука

--- Assets/Scripts/Sound/AudioManager.cs
+++ Assets/Scripts/Sound/AudioManager.cs
@@ -28,7 +28,7 @@ namespace CastleFight
         public void Play(string name)
         {
             Sound s = Array.Find(sounds, sound => sound.name == name);
-            s.source.Play();
+            if(s != null) s.source.Play();
             Debug.Log("Playing: " + name);
         }
 
