7bba507ac78659b3af5679a6213ca07a4a6e7cb1
Andrey <unitydeveloper7@gmail.com>
2020-09-21 20:25:11 +0300

    Добавил привидение 3 уровня

---
+++ Assets/Scripts/Config/Units/Ghosts/GhostLevel3Config.cs
@@ -0,0 +1,26 @@
+﻿using CastleFight.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CastleFight.Config
+{
+    [CreateAssetMenu(fileName = "GhostLevel3", menuName = "Units/GhostLevel3", order = 0)]
+    public class GhostLevel3Config : BaseUnitConfig
+    {
+        public override Unit Create(Team team)
+        {
+            var unit = Pool.I.Get<GhostLevel3>();
+
+            if (unit == null)
+            {
+                unit = Instantiate(prefab) as GhostLevel3;
+                unit.Init(this, team);
+                unit.gameObject.SetActive(true);
+            }
+
+            unit.gameObject.layer = (int)team;
+            return unit;
+        }
+    }
+}
