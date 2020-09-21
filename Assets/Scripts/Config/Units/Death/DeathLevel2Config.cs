2f92fbcbf2287a6c2570006d172d22315af195a1
Andrey <unitydeveloper7@gmail.com>
2020-09-22 00:38:20 +0300

    Добавил умертвие 2 уровня

---
+++ Assets/Scripts/Config/Units/Death/DeathLevel2Config.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CastleFight.Core;
+using UnityEngine;
+
+namespace CastleFight.Config
+{
+    [CreateAssetMenu(fileName = "DeathLevel2", menuName = "Units/DeathLevel2", order = 0)]
+    public class DeathLevel2Config : BaseUnitConfig
+    {
+        public override Unit Create(Team team)
+        {
+            var unit = Pool.I.Get<DeathLevel2>();
+
+            if (unit == null)
+            {
+                unit = Instantiate(prefab) as DeathLevel2;
+                unit.Init(this, team);
+                unit.gameObject.SetActive(true);
+            }
+
+            unit.gameObject.layer = (int)team;
+            return unit;
+        }
+    }
+}
